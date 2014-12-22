787303b5e33a15a5088bf9d92856c306417d5f91
Ningxin Hu <ningxin.hu@intel.com>
2014-12-22 17:25:45 +0800

    Add makefile for asm.js

---
+++ benchcpp.asmjs.mk
@@ -0,0 +1,20 @@
+EMCC=$(EMSCRIPTEN)/emcc -DNDEBUG=1 -I./
+HEAP=570425344
+
+OBJECTS = \
+run.bc \
+base.bc
+
+all: benchcpp.js
+
+%.bc: %.cpp
+	$(EMCC) -O2 -g -s ASM_JS=1 $< -o $@
+
+benchcpp.js.bc: $(OBJECTS)
+	$(LLVM)/llvm-link -o $@ $(OBJECTS)
+
+benchcpp.js: benchcpp.js.bc
+	$(EMCC) -O2 -g -s ASM_JS=1 -s TOTAL_MEMORY=$(HEAP) $< -o $@
+
+clean:
+	rm benchcpp.js benchcpp.js.bc $(OBJECTS)
